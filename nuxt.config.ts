3d9d84c86dafa8d262362024ff093995a27461e9
Antony <s061402@gmail.com>
2024-12-12 23:48:43 +0800

    Update HTTPS - Morris

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
-  modules: ['@nuxtjs/strapi', '@pinia/nuxt', 'nuxt-tiptap-editor', '@vesp/nuxt-fontawesome', '@nuxt/image', '@hypernym/nuxt-gsap', 'nuxt-particles', 'nuxt-bootstrap-icons', '@nuxt/fonts'],
+  modules: ['@nuxtjs/strapi', '@pinia/nuxt', 'nuxt-tiptap-editor', '@vesp/nuxt-fontawesome', // '@vueform/builder-nuxt' // IT NEED KEYS
+  '@vueform/nuxt', '@nuxt/image'],
 
   css: [
     'bootstrap/dist/css/bootstrap.min.css',
     '@oruga-ui/theme-bootstrap/dist/bootstrap.css',
-    'bootstrap-icons/font/bootstrap-icons.min.css',
 
     '@fortawesome/fontawesome-free/css/all.min.css',
     'font-awesome/css/font-awesome.css',
@@ -14,9 +14,6 @@ export default defineNuxtConfig({
     //Shall put Bootstrap Studio CSS order at Last
     '/assets/bootstrap/css/bootstrap.min.css',
     '/assets/fonts/font-awesome.min.css',
-
-    '/assets/css/_variables.css', //My css for navbar
-    '/assets/css/styles.min.css', //My css for BS components
 
   ],
 
@@ -35,7 +32,7 @@ export default defineNuxtConfig({
   },
 
   strapi: {
-    url: process.env.STRAPI_URL || 'http://158.182.151.62:1337',
+    url: process.env.STRAPI_URL || 'https://profile-cms.sci.hkbu.edu.hk',
     prefix: '/api',
     admin: '/admin',
     version: 'v5',
@@ -58,7 +55,5 @@ export default defineNuxtConfig({
     }
   },
 
-  ssr: false, //Fix for Firefox // Bootstrap... 
-
-  // STRAPI TOKEN: 8a2a82b58882d99f781af813ec54b0bdfe773dd755c337081a22f07b33b81875005fb6004ffe9e08c575ff66e4d5f72645cd28286ed1307e70467ff65a6dbb804d8ce0c972515357c7b8693600380878fb497a2dff49f32e3da5ccdf3972bcb7b23781570af4c55fa1a690c344fa8ff430e689a946425dfcca7ac80c5f48d3d1
+  // ssr: false, //Fix for Firefox...
 })
