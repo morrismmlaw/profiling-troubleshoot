17fd4fd878035c5e10b797e4f68be6b8c9860e38
Antony <s061402@gmail.com>
2024-12-13 00:39:29 +0800

    Add Test Search Page

---
+++ pages/search.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="nav-offset">
+
+    <div>
+      <h1>Search Results</h1>
+      <p>You searched for: {{ $route.query.q }}</p>
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
