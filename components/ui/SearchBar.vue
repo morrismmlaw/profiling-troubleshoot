8ce9a8fbfcbd6bd4eded3950f3b134c1f01d74ab
Antony <s061402@gmail.com>
2024-12-13 23:06:56 +0800

    Update Search Cancel Button

--- components/ui/SearchBar.vue
+++ components/ui/SearchBar.vue
@@ -62,4 +62,13 @@ const handleSearch = () => {
 }
 </script>
 
-<style></style>
+<style scoped>
+input[type="search"]::-webkit-search-cancel-button {
+  -webkit-appearance: none;
+  display: inline-block;
+  width: 25px;
+  height: 25px;
+  /* margin-left: 10px; */
+  background: url("data:image/svg+xml;charset=UTF-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='25' height='25' fill='currentColor' class='bi bi-x-circle-fill' viewBox='-7 -5 25 25'%3E%3Cpath d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z'/%3E%3C/svg%3E");
+}
+</style>
