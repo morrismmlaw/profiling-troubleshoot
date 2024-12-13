a81f1b85765c05d800a762674679310cfd3203ff
chan <s061402@gmail.com>
2024-12-13 17:06:07 +0800

    Add Back Offfcanvas Backrop after Modal Fix

--- components/ui/Navbar.vue
+++ components/ui/Navbar.vue
@@ -26,13 +26,12 @@
         </NuxtLink>
       </div>
 
-
       <button class="navbar-toggler bg-body-secondary semi-transparent" type="button" data-bs-toggle="offcanvas"
         data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="offcanvas offcanvas-end semi-transparent-1" tabindex="-1" id="offcanvasNavbar"
-        aria-labelledby="offcanvasNavbarLabel" data-bs-backdrop="false">
+        aria-labelledby="offcanvasNavbarLabel">
         <div class="offcanvas-header">
           <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
             <span class="d-flex align-items-center flex-nowrap ">
