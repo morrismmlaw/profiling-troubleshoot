425caf79f827340a8fb59129251f769a8779c659
chan <s061402@gmail.com>
2024-12-18 13:33:11 +0800

    Code Clean up

--- components/ProfileForm.vue
+++ components/ProfileForm.vue
@@ -396,17 +396,14 @@ const showBooks = ref(false);
                       <div class="columns is-multiline">
                         <div class="column is-one-fifth" v-for="sdg in sdgOptions" :key="sdg">
                           <o-field class="sdg-field">
-                            <!-- <o-tooltip :label="`SDG: ${sdg.sdgid} \n\n Slogan: ${sdg.slogan}`" multiline> -->
                             <o-tooltip label="HTML Content" size="large" variant="info" multiline>
                               <template #content>
                                 <div class="sdg-info">
-                                  <!-- <p><strong>SDG:</strong> {{ sdg.sdgid }}</p> -->
                                   <p><strong>Title</strong> <br> {{ sdg.title }}</p>
                                   <p><strong>Description</strong> <br> {{ sdg.slogan }}</p>
                                 </div>
                               </template>
                               <o-checkbox v-model="checkboxGroup" :native-value="`${sdg.sdgid}`" :label="sdg.sdgid">
-                                <!-- <o-checkbox v-model="checkboxGroup" :native-value="sdg" :label="sdg.sdgid"> -->
                                 <p class="col-form-label-sdg"> SDG {{ sdg.sdgid }} </p>
                                 <nuxt-img
                                   :src="`https://edu.unicef.org.hk/image/catalog/teaching%20resource/goal${sdg.sdgid}a.png`"
@@ -441,7 +438,6 @@ const showBooks = ref(false);
                         :allow-duplicates="false" :open-on-focus="openOnFocus" :keep-open="false"
                         :keep-first="keepFirst" icon="tag" placeholder="Select options" expanded />
                     </o-field>
-                    <!-- <p><b>FCRAS:</b> {{ fcraTags }}</p> -->
                   </section>
                   <section>
                     <o-field class="col-form-tag" label="RESEARCH FOCUS">
@@ -449,7 +445,6 @@ const showBooks = ref(false);
                         :allow-duplicates="false" :open-on-focus="openOnFocus" :keep-open="false"
                         :keep-first="keepFirst" icon="tag" placeholder="Select options" expanded />
                     </o-field>
-                    <!-- <p><b>RESEARCH FOCUS:</b> {{ RSTags }}</p> -->
                   </section>
                   <section>
                     <o-field class="col-form-tag" label="FACULTY COLLABORATIVE RESEARCH AREA">
@@ -484,93 +479,6 @@ const showBooks = ref(false);
 
           </o-tabs>
         </section>
-
-        <!-- <div class="card ms-md-0 ms-2 shadow rounded-5 border-0">
-          <div class="card-body">
-            <div class="row mb-3 rounded-5 border-4">
-              <div class="row">
-                <label for="biography" class="col col-form-label">Biography</label>
-              </div>
-              <div class="row">
-                <div class="col">
-                  <tiptap-editor :formData="formData" field="biography" />
-                </div>
-              </div>
-            </div>
-            <div class="row mb-3">
-              <div class="row">
-                <label for="academicInterests" class="col col-form-label">Academic Interests</label>
-              </div>
-              <div class="row">
-                <div class="col">
-                  <tiptap-editor :formData="formData" field="research_interest" />
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-12 col-sm-12 col-md-12">
-                <h6 class="col-form-label">Sustainable Development Goals (SDGs)</h6>
-              </div>
-              <div class="col-11 col-sm-11 col-md-12">
-                <div class="columns is-multiline">
-                  <div class="column is-one-fifth" v-for="sdg in sdgOptions" :key="sdg">
-                    <o-field class="sdg-field">
-                      <o-tooltip label="HTML Content" size="large" variant="info" multiline>
-                        <template #content>
-                          <div class="sdg-info">
-                            <p><strong>Title</strong> <br> {{ sdg.title }}</p>
-                            <p><strong>Description</strong> <br> {{ sdg.slogan }}</p>
-                          </div>
-                        </template>
-                        <o-checkbox v-model="checkboxGroup" :native-value="`${sdg.sdgid}`" :label="sdg.sdgid">
-                          <p class="col-form-label-sdg"> SDG {{ sdg.sdgid }} </p>
-                          <nuxt-img
-                            :src="`https://edu.unicef.org.hk/image/catalog/teaching%20resource/goal${sdg.sdgid}a.png`"
-                            :alt="sdg.title" class="img-fluid checkbox-img" />
-                        </o-checkbox>
-                      </o-tooltip>
-                    </o-field>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <section>
-              <o-field class="col-form-tag" label="DEPARTMENT">
-                <o-taginput v-model="DEPTags" :options="DEPOptionsOrugaNew" :allow-new="allowNew"
-                  :allow-duplicates="false" :open-on-focus="openOnFocus" :keep-open="false" :keep-first="keepFirst"
-                  icon="tag" placeholder="Select options" expanded />
-              </o-field>
-            </section>
-            <section>
-              <o-field class="col-form-tag" label="STRATEGIC RESEARCH CENTRE">
-                <o-taginput v-model="SRCTags" :options="SRCOptionsOrugaNew" :allow-new="allowNew"
-                  :allow-duplicates="false" :open-on-focus="openOnFocus" :keep-open="false" :keep-first="keepFirst"
-                  icon="tag" placeholder="Select options" expanded />
-              </o-field>
-            </section>
-            <section>
-              <o-field class="col-form-tag" label="RESEARCH FOCUS">
-                <o-taginput v-model="RFTags" :options="RFOptionsOrugaNew" :allow-new="allowNew"
-                  :allow-duplicates="false" :open-on-focus="openOnFocus" :keep-open="false" :keep-first="keepFirst"
-                  icon="tag" placeholder="Select options" expanded />
-              </o-field>
-            </section>
-            <section>
-              <o-field class="col-form-tag" label="FACULTY COLLABORATIVE RESEARCH AREA">
-                <o-taginput v-model="FCRATags" :options="FCRAOptionsOrugaNew" :allow-new="allowNew"
-                  :allow-duplicates="false" :open-on-focus="openOnFocus" :keep-open="false" :keep-first="keepFirst"
-                  icon="tag" placeholder="Select options" expanded />
-              </o-field>
-            </section>
-            <section>
-              <o-field class="col-form-tag" label="AVAILABILITY FOR SUPERVISION">
-                <o-taginput v-model="ASTags" :options="ASOptionsOrugaNew" :allow-new="allowNew"
-                  :allow-duplicates="false" :open-on-focus="openOnFocus" :keep-open="false" :keep-first="keepFirst"
-                  icon="tag" placeholder="Select options" expanded />
-              </o-field>
-            </section>
-          </div>
-        </div> -->
 
         <form @submit.prevent="handleSubmit">
           <div class="d-flex justify-content-end mt-4">
