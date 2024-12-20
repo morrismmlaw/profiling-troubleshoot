e486b49ae990dee8ca503c84c8134cbb7cefe1d9
chan <s061402@gmail.com>
2024-12-20 14:41:41 +0800

    Refractor Save Form button

---
+++ components/ui/ProfileSave.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="mb-4">
+      <form @submit.prevent="handleSubmit">
+        <div class="d-flex justify-content-end mt-4">
+          <button type="submit" class="btn btn-primary" :disabled="profileStore.isLoading">
+            {{ profileStore.isLoading ? 'Saving...' : 'Save' }}
+          </button>
+          <p v-if="profileStore.error" class="text-danger mt-3">{{ profileStore.error }}</p>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+import { useProfileStore } from '#build/imports';
+
+defineProps({
+  handleSubmit: {
+    type: Function as PropType<(event: Event) => void>,
+    required: true
+  },
+  profileStore: {
+    type: useProfileStore,
+    required: true
+  }
+})
+
+</script>
+
+<style></style>
