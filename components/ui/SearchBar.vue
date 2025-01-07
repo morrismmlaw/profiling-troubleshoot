58bda932ac5aeb70b012d9babf754012eeb4ff79
chan <s061402@gmail.com>
2025-01-07 14:12:01 +0800

    Enhance Positon and shadow

--- components/ui/SearchBar.vue
+++ components/ui/SearchBar.vue
@@ -2,11 +2,11 @@
   <div>
     <form @submit.prevent="handleSearch">
       <div id="myInputSearch"
-        style="margin-left: 1px;background: var(--bs-light);border-radius: 41px;/*box-shadow: inset 1px 1px 5px 1px;*/margin-top: 2px;height: 53px;border-width: thin;border-style: solid;">
+        style="margin-left: 1px;background: var(--bs-light);border-radius: 41px;box-shadow: 0px 1px 10px 1px;margin-top: 2px;height: 53px;border-width: thin;border-style: solid;">
         <label class="form-label" style="width: 100%;margin-top: 0px;">
           <NuxtLink to="/search" @click="handleSearch">
             <svg class="text-black icon-hover" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em"
-              viewBox="-2 -3 30 30" fill="none"
+              viewBox="-1 -3 30 30" fill="none"
               style="margin-left: 14px;font-size: 30px;margin-top: -3px;color: var(--bs-secondary);">
               <path fill-rule="evenodd" clip-rule="evenodd"
                 d="M18.319 14.4326C20.7628 11.2941 20.542 6.75347 17.6569 3.86829C14.5327 0.744098 9.46734 0.744098 6.34315 3.86829C3.21895 6.99249 3.21895 12.0578 6.34315 15.182C9.22833 18.0672 13.769 18.2879 16.9075 15.8442C16.921 15.8595 16.9351 15.8745 16.9497 15.8891L21.1924 20.1317C21.5829 20.5223 22.2161 20.5223 22.6066 20.1317C22.9971 19.7412 22.9971 19.1081 22.6066 18.7175L18.364 14.4749C18.3493 14.4603 18.3343 14.4462 18.319 14.4326ZM16.2426 5.28251C18.5858 7.62565 18.5858 11.4246 16.2426 13.7678C13.8995 16.1109 10.1005 16.1109 7.75736 13.7678C5.41421 11.4246 5.41421 7.62565 7.75736 5.28251C10.1005 2.93936 13.8995 2.93936 16.2426 5.28251Z"
@@ -22,7 +22,7 @@
                           </i> -->
             <div class="dropdown">
               <svg class="text-black icon-hover dropdown-toggle" type="button" data-bs-toggle="dropdown"
-                xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="-3 -3 30 30" fill="none"
+                xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="-4 -2 30 30" fill="none"
                 style="font-size: 30px;margin-top: -4px;color: var(--bs-secondary);margin-left: 8px;">
                 <path
                   d="M2 5.99519C2 5.44556 2.44556 5 2.99519 5H11.0048C11.5544 5 12 5.44556 12 5.99519C12 6.54482 11.5544 6.99039 11.0048 6.99039H2.99519C2.44556 6.99039 2 6.54482 2 5.99519Z"
