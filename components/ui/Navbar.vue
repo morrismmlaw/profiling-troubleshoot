c2449af1a848009aa13474e13ae2939c3791e7e5
chan <s061402@gmail.com>
2025-01-08 15:49:30 +0800

    Navbar Enhance

--- components/ui/Navbar.vue
+++ components/ui/Navbar.vue
@@ -4,83 +4,84 @@
     style="position: absolute; top: 0; left: 0; width: 100%; z-index: 2;">
     <div class="container-fluid">
 
-      <div v-if="!isSearch && !isProfile" class="d-flex">
-
-        <NuxtLink class="navbar-brand" to="/">
-          <NuxtImg src="\img\bu-logo-white.f7871964.png" alt="Logo" class="logo-img-1 ms-4" />
-        </NuxtLink>
-        <NuxtLink class="navbar-brand" to="/">
-          <NuxtImg src="\img\scilogo_v3_White_1012ver.e2d2cba4.png" alt="Logo" class="logo-img-2" />
-        </NuxtLink>
-
+      <div v-if="!isSearch && !isProfile">
+        <div class="d-flex">
+          <NuxtLink class="navbar-brand" to="/">
+            <NuxtImg src="\img\bu-logo-white.f7871964.png" alt="Logo" class="logo-img-1 ms-4" />
+          </NuxtLink>
+          <NuxtLink class="navbar-brand" to="/">
+            <NuxtImg src="\img\scilogo_v3_White_1012ver.e2d2cba4.png" alt="Logo" class="logo-img-2" />
+          </NuxtLink>
+        </div>
       </div>
 
-      <div v-else class="d-flex">
-
-        <NuxtLink class="navbar-brand" to="/">
-          <NuxtImg src="\img\bu-logo.d3e1d17a.png" alt="Logo" class="logo-img-1 ms-4" />
-        </NuxtLink>
-        <NuxtLink class="navbar-brand" to="/">
-          <NuxtImg src="\img\scilogo_v3_1012ver.7b173d33.png" alt="Logo" class="logo-img-2" />
-        </NuxtLink>
-
-        <div v-if="$route.path === '/profile'" class="d-flex align-items-center ms-1">
-          {{ console.log("is in profile") }}
-          <div class="custom-text-bar"> Academic Profile</div>
+      <div v-else>
+        <div class="d-flex">
+          <NuxtLink class="navbar-brand" to="/">
+            <NuxtImg src="\img\bu-logo.d3e1d17a.png" alt="Logo" class="logo-img-1 ms-4" />
+          </NuxtLink>
+          <NuxtLink class="navbar-brand" to="/">
+            <NuxtImg src="\img\scilogo_v3_1012ver.7b173d33.png" alt="Logo" class="logo-img-2" />
+          </NuxtLink>
         </div>
-
       </div>
 
-      <button class="navbar-toggler bg-body-secondary semi-transparent" type="button" data-bs-toggle="offcanvas"
-        data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="offcanvas offcanvas-end semi-transparent-1" tabindex="-1" id="offcanvasNavbar"
-        aria-labelledby="offcanvasNavbarLabel">
-        <div class="offcanvas-header">
-          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
-            <span class="d-flex align-items-center flex-nowrap ">
-              <i class="bi bi-list me-3"> </i>
-              Menu
-            </span>
-          </h5>
-          <button type="button" class="btn-close" style="width: 3rem; height: 2rem; margin-right: 12px;"
-            data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body">
-          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-            <li class="nav-item custom-nav-item">
-              <span class="d-flex align-items-center flex-nowrap" data-bs-dismiss="offcanvas">
-                <i class="bi bi-house me-3"></i>
-                <NuxtLink class="nav-link" to="/">Home</NuxtLink>
+      <div v-if="$route.path === '/profile'" class="d-flex justify-content-end ms-auto me-3">
+        <div class="custom-text-bar"> Academic Profile</div>
+      </div>
+
+      <div class="d-flex">
+        <button class="navbar-toggler bg-body-secondary semi-transparent" type="button" data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end semi-transparent-1" tabindex="-1" id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
+              <span class="d-flex align-items-center flex-nowrap ">
+                <i class="bi bi-list me-3"> </i>
+                Menu
               </span>
-            </li>
-            <li v-if="!isLoggedIn" class="nav-item custom-nav-item">
-              <span class="d-flex align-items-center flex-nowrap" data-bs-dismiss="offcanvas">
-                <i class="bi bi-box-arrow-in-right me-3" style="margin-left: -3px;"></i>
-                <NuxtLink class="nav-link" to="/login">Staff Login</NuxtLink>
-              </span>
-            </li>
-            <!-- Need to Check if the current User has Academic Profile / is Not an Admin -->
-            <li v-if="isLoggedIn && isAuthenticated" class="nav-item custom-nav-item">
-              <span class="d-flex align-items-center flex-nowrap" data-bs-dismiss="offcanvas">
-                <i class="bi bi-person-circle me-3"></i>
-                <NuxtLink class="nav-link" to="/profile"> Your Profile</NuxtLink>
-              </span>
-            </li>
-            <li v-if="isLoggedIn && isAdmin" class="nav-item custom-nav-item">
-              <span class="d-flex align-items-center flex-nowrap" data-bs-dismiss="offcanvas">
-                <i class="bi bi-people-fill me-3"></i>
-                <NuxtLink class="nav-link" to="/profile"> View All Profiles</NuxtLink>
-              </span>
-            </li>
-            <li v-if="isLoggedIn" class="nav-item custom-nav-item">
-              <span class="d-flex align-items-center flex-nowrap" data-bs-dismiss="offcanvas">
-                <i class=" bi bi-box-arrow-left me-3"></i>
-                <NuxtLink @click="logout" class="nav-link" to="/"> Logout</NuxtLink>
-              </span>
-            </li>
-          </ul>
+            </h5>
+            <button type="button" class="btn-close" style="width: 3rem; height: 2rem; margin-right: 12px;"
+              data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li class="nav-item custom-nav-item">
+                <span class="d-flex align-items-center flex-nowrap" data-bs-dismiss="offcanvas">
+                  <i class="bi bi-house me-3"></i>
+                  <NuxtLink class="nav-link" to="/">Home</NuxtLink>
+                </span>
+              </li>
+              <li v-if="!isLoggedIn" class="nav-item custom-nav-item">
+                <span class="d-flex align-items-center flex-nowrap" data-bs-dismiss="offcanvas">
+                  <i class="bi bi-box-arrow-in-right me-3" style="margin-left: -3px;"></i>
+                  <NuxtLink class="nav-link" to="/login">Staff Login</NuxtLink>
+                </span>
+              </li>
+              <!-- Need to Check if the current User has Academic Profile / is Not an Admin -->
+              <li v-if="isLoggedIn && isAuthenticated" class="nav-item custom-nav-item">
+                <span class="d-flex align-items-center flex-nowrap" data-bs-dismiss="offcanvas">
+                  <i class="bi bi-person-circle me-3"></i>
+                  <NuxtLink class="nav-link" to="/profile"> Your Profile</NuxtLink>
+                </span>
+              </li>
+              <li v-if="isLoggedIn && isAdmin" class="nav-item custom-nav-item">
+                <span class="d-flex align-items-center flex-nowrap" data-bs-dismiss="offcanvas">
+                  <i class="bi bi-people-fill me-3"></i>
+                  <NuxtLink class="nav-link" to="/profile"> View All Profiles</NuxtLink>
+                </span>
+              </li>
+              <li v-if="isLoggedIn" class="nav-item custom-nav-item">
+                <span class="d-flex align-items-center flex-nowrap" data-bs-dismiss="offcanvas">
+                  <i class=" bi bi-box-arrow-left me-3"></i>
+                  <NuxtLink @click="logout" class="nav-link" to="/"> Logout</NuxtLink>
+                </span>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
@@ -225,5 +226,9 @@ const navbarStylePerRoute = computed(() => {
   /* padding: 1rem; */
   padding-top: 0.5rem;
 
+}
+
+.navbar-toggler {
+  padding: 1rem;
 }
 </style>
