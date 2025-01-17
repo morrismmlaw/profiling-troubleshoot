5713ceb7ab10032d21de724e5ddfa88246ca149b
chan <s061402@gmail.com>
2025-01-17 15:16:34 +0800

    Code Clean

--- components/ui/ExpertCard.vue
+++ components/ui/ExpertCard.vue
@@ -6,22 +6,9 @@
         <div class="row shadow-sm mb-2 rounded" style="margin-top: 5px;">
           <div class="col-md-3 col-lg-3 col-xxl-3" style="padding-top: 29px;">
             <div style="padding: 30px;padding-top: 0px;margin-right: 0px;margin-top: 10px;margin-left: -20px;">
-
               <div class="profile-card">
                 <NuxtImg :src="props.profile.photoURL" class="expert-card-image"></NuxtImg>
               </div>
-
-              <!-- <svg
-                  xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none"
-                  style="font-size: 180px;border-radius: 80px;">
-                  <path fill-rule="evenodd" clip-rule="evenodd"
-                    d="M16 9C16 11.2091 14.2091 13 12 13C9.79086 13 8 11.2091 8 9C8 6.79086 9.79086 5 12 5C14.2091 5 16 6.79086 16 9ZM14 9C14 10.1046 13.1046 11 12 11C10.8954 11 10 10.1046 10 9C10 7.89543 10.8954 7 12 7C13.1046 7 14 7.89543 14 9Z"
-                    fill="currentColor"></path>
-                  <path fill-rule="evenodd" clip-rule="evenodd"
-                    d="M12 1C5.92487 1 1 5.92487 1 12C1 18.0751 5.92487 23 12 23C18.0751 23 23 18.0751 23 12C23 5.92487 18.0751 1 12 1ZM3 12C3 14.0902 3.71255 16.014 4.90798 17.5417C6.55245 15.3889 9.14627 14 12.0645 14C14.9448 14 17.5092 15.3531 19.1565 17.4583C20.313 15.9443 21 14.0524 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12ZM12 21C9.84977 21 7.87565 20.2459 6.32767 18.9878C7.59352 17.1812 9.69106 16 12.0645 16C14.4084 16 16.4833 17.1521 17.7538 18.9209C16.1939 20.2191 14.1881 21 12 21Z"
-                    fill="currentColor"></path>
-                </svg> -->
-
               <label class="form-label" style="padding-left: 35px;padding-top: 7px;"><svg class="bi bi-hdd-network"
                   xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16"
                   style="font-size: 20px;margin-right: 5px;">
