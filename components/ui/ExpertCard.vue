696fef4b2d73594a77c5e58d10a4f55885847173
chan <s061402@gmail.com>
2025-05-15 15:49:38 +0800

    Update ExpertCard component to display matched fields in search results; enhance Profile interface with additional research interests and awards

--- components/ui/ExpertCard.vue
+++ components/ui/ExpertCard.vue
@@ -124,7 +124,11 @@
                           <path fill-rule="evenodd" clip-rule="evenodd"
                             d="M8 10C5.79086 10 4 11.7909 4 14C4 14.7414 4.20229 15.4364 4.55397 16.0318L3.29289 17.2929C2.90237 17.6834 2.90237 18.3166 3.29289 18.7071C3.68342 19.0976 4.31658 19.0976 4.70711 18.7071L5.96818 17.446C6.56362 17.7977 7.25862 18 8 18C10.2091 18 12 16.2091 12 14C12 11.7909 10.2091 10 8 10ZM6 14C6 12.8954 6.89543 12 8 12C9.10457 12 10 12.8954 10 14C10 15.1046 9.10457 16 8 16C7.44744 16 6.94881 15.7772 6.58579 15.4142C6.22276 15.0512 6 14.5526 6 14Z"
                             fill="currentColor"></path>
-                        </svg>Your search matched against this person&#39;s publications.</small></label></div>
+                        </svg>Your search matched against this person&#39;s {{ 
+
+                        matchedFieldsName 
+                        
+                        }}.</small></label></div>
                 </div>
               </div>
             </div>
@@ -160,6 +164,8 @@ interface Profile {
   biography?: string;
   research_foci: ResearchItem[];
   research_centres: ResearchItem[];
+  research_interest: ResearchItem[];
+  research_awards: ResearchItem[];
   departments: ResearchItem[];
   available_supervisions: ResearchItem[];
   fcras: ResearchItem[];
@@ -216,6 +222,10 @@ const matchedFields = computed(() => {
   console.log('Cleaned Matched Fields:', result);
 
   return result
+});
+
+const matchedFieldsName = computed(() => {
+  return matchedFields.value.map(item => item.field).join(', ');
 });
 
 // let customPhotoURL = 'https://profile-cms.sci.hkbu.edu.hk' + props.profile.uploadPhoto;
